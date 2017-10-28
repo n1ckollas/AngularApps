67f8d0200eec96a3c74a137ce2b89f8a1a99493e
n1ckollas <nick.rebez@gmail.com>
2017-10-28 15:43:06 -0400

    Weather app api setup

--- app/components/weather-app/weather/weather.component.ts
+++ app/components/weather-app/weather/weather.component.ts
@@ -7,10 +7,17 @@ import { WeatherService } from "../../../services/weather-app/weather.service";
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+	weatherData:any;
 
-  constructor(private weather:WeatherService) { }
+  constructor(private weather:WeatherService) { 
+
+  }
 
   ngOnInit() {
+  	this.weather.getWeather('New_York', 'NY').subscribe(res => {
+  		this.weatherData = res;
+  		console.log(res);
+  	})
   }
 
 }
