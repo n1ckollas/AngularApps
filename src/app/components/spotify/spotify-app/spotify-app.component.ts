4cbb1bcb9cf22952f331aa4970b6f5e5354f9fec
n1ckollas <nick.rebez@gmail.com>
2017-11-22 17:45:49 -0500

    Light html stiling

--- src/app/components/spotify/spotify-app/spotify-app.component.ts
+++ src/app/components/spotify/spotify-app/spotify-app.component.ts
@@ -7,7 +7,7 @@ import { SpotifyService } from '../../../services/spotify/spotify.service';
 })
 export class SpotifyAppComponent implements OnInit {
 	responce:boolean = false;
-  artists:any;
+  searchTerms:any;
   artist = '';
 
   constructor(public sp:SpotifyService) {}
@@ -18,12 +18,12 @@ export class SpotifyAppComponent implements OnInit {
 
   artistsSearch(artist:string){
   	this.sp.getArtists(artist).subscribe(res => {
-  		this.artists = res;
+  		this.searchTerms = res;
       this.responce = true;
       console.log('search for:');
       console.log(artist);
       console.log('returns');
-  		console.log(this.artists);
+  		console.log(this.searchTerms);
   	})
   }
 
