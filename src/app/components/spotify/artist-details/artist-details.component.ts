409abfcda9fe4afb877f53f82eec1be1908d6f6a
n1ckollas <nick.rebez@gmail.com>
2017-12-02 16:03:33 -0500

    audio element reworked to display in older brwsrs.

--- src/app/components/spotify/artist-details/artist-details.component.ts
+++ src/app/components/spotify/artist-details/artist-details.component.ts
@@ -13,6 +13,8 @@ export class ArtistDetailsComponent implements OnInit {
 	id:string;
 	albums:any;
   hidden:boolean = false;
+  divId:any;
+  bId:any;
 
   constructor(public route:ActivatedRoute, public sp:SpotifyService) {
   	this.id = this.route.snapshot.paramMap.get('id');
@@ -27,39 +29,38 @@ export class ArtistDetailsComponent implements OnInit {
   	this.sp.getArtistDetails(id).subscribe(res =>{
   		this.albums = res;
   	})
+
+    
   }
 
   getTracks(album_id:string){
-    this.hidden = false;
     var tracks;
-    var divId = document.getElementById(album_id);
-
+    this.divId = document.getElementById(album_id);
+    this.bId   = document.getElementById('b-'+ album_id);
   	this.sp.getAlbumTracks(album_id).subscribe(res => {
   		tracks = res;
       for (var i = tracks.items.length - 1; i >= 0; i--) {
-        divId.innerHTML += '<div class="card-footer"'+
-                              '<p>Name: '+ tracks.items[i].name +'</p>'+
-                              '<audio controls>' +
-                                '<source src="'+ tracks.items[i].preview_url +'" type="audio/ogg">'+
-                                '<source src="'+ tracks.items[i].preview_url +'" type="audio/mpeg">'+
-                                'Your browser doesnt support this. But chrome does.'+
-                              '</audio>'+
-                            '</div>';
+        var audio  = document.createElement("audio");
+        var footer = document.createElement("div");
+        footer.classList.add('card-footer');
+        audio.controls = true;
+        audio.preload = "auto";
+        audio.src = tracks.items[i].preview_url
+        footer.appendChild(audio);
+        this.divId.appendChild(footer);
       }
   	})
   }
 
   hideTracks(album_id:string){
-    var divId = document.getElementById(album_id);
-    var bId = document.getElementById('b-'+ album_id);
     if(this.hidden === false){
       this.hidden = true;
-      divId.classList.add('hideMe');
-      bId.innerText = "Show 'em";
+      this.divId.classList.add('hideMe');
+      this.bId.innerText = "Show 'em";
     }else if(this.hidden === true){
       this.hidden = false;
-      divId.classList.remove('hideMe');
-      bId.innerText = "Hide 'em";
+      this.divId.classList.remove('hideMe');
+      this.bId.innerText = "Hide 'em";
     }
   }
 
